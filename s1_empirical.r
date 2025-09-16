faefd5321229c00175c51b4be8dde7f1d066f998
Mingya Huang <mingyah2@icloud.com>
2025-09-16 14:08:28 -0500

    codes for study 1

---
+++ s1_empirical.r
@@ -0,0 +1,286 @@
+# ::: STUDY 1: EMPIRICAL ::: #
+
+# Setup and libraries
+{
+  library(loo)
+  library(rstanarm)
+  library(rstan)
+  library(LaplacesDemon)
+  library(kableExtra)
+  library(bayesplot)
+  library(tidyverse)
+  options(mc.cores = parallel::detectCores())
+  set.seed(53705)
+}
+
+# Load PISA 2018 data
+df0 <- read.csv("pisa2018.BayesBook.csv")
+
+df <- df0 %>%
+  dplyr::select(SchoolID, CNTSTUID, Female, ESCS, METASUM, PERFEED, HOMEPOS, 
+                ADAPTIVITY, TEACHINT, ICTRES, ATTLNACT, COMPETE, JOYREAD,
+                WORKMAST, GFOFAIL, SWBP, MASTGOAL, BELONG, SCREADCOMP, 
+                PISADIFF, Public, PV1READ, SCREADDIFF)
+
+# Filter schools >10 students
+sch <- table(df$SchoolID)
+dt0 <- subset(df, SchoolID %in% names(sch[sch > 10]))
+
+# Check final school distribution
+dt0 %>%
+  group_by(SchoolID) %>%
+  summarise(n=n())
+
+# ::: REDUCED SAMPLE ANALYSIS ::: #
+# Sample 10 students per school, then select 50 schools
+dt <- dt0 %>% group_by(SchoolID) %>% slice_sample(n = 10)
+SchID <- dt$SchoolID 
+unique_sch <- unique(dt$SchoolID)
+
+sch_sample <- sample(unique_sch, 50)
+sch_index <- which(SchID %in% sch_sample)
+
+df <- dt[sch_index, ]
+
+# Fit four candidate models
+bsm <- list()
+loo_bs <- list()
+
+# Model 1: Demographic perspective
+bsm[[1]] <- stan_lmer(
+  PV1READ ~ Female + ESCS + HOMEPOS + ICTRES + (1 + ICTRES|SchoolID), data = df, 
+  prior_intercept = student_t(3, 470, 100),
+  iter = 10000, chains = 4, adapt_delta=.999, thin=10)
+
+# Model 2: Reading behavior perspective
+bsm[[2]] <- stan_lmer(
+  PV1READ ~ JOYREAD + PISADIFF + SCREADCOMP + SCREADDIFF + (1|SchoolID),
+  data = df, prior_intercept = student_t(3, 470, 100), iter = 10000, chains = 4,
+  adapt_delta=.999, thin=10)
+
+# Model 3: Academic mindset perspective
+bsm[[3]] <- stan_lmer(
+  PV1READ ~ METASUM + GFOFAIL + MASTGOAL + SWBP + WORKMAST + ADAPTIVITY + COMPETE + (1|SchoolID),
+  data = df, prior_intercept = student_t(3, 470, 100), iter = 10000, chains = 4,
+  adapt_delta=.999, thin=10)
+
+# Model 4: School climate perspective
+bsm[[4]] <- stan_lmer(
+  PV1READ ~ PERFEED + TEACHINT + BELONG + (1 + TEACHINT|SchoolID),
+  data = df, prior_intercept = student_t(3, 470, 100), iter = 10000, chains = 4,
+  adapt_delta=.999, thin=10)
+
+# Compute LOO
+loo_bs[[1]] <- loo(log_lik(bsm[[1]]))
+loo_bs[[2]] <- loo(log_lik(bsm[[2]]))
+loo_bs[[3]] <- loo(log_lik(bsm[[3]]))
+loo_bs[[4]] <- loo(log_lik(bsm[[4]]))
+
+# Compute ensemble weights dor stakcing-based methods
+system.time(w_bs <- loo_model_weights(loo_bs, method = "stacking"))
+system.time(w_pbma <- loo_model_weights(loo_bs, method = "pseudobma", BB=FALSE))
+system.time(w_pbmabb <- loo_model_weights(loo_bs, method = "pseudobma"))
+
+# Extract pointwise LOO densities
+lpd_point <- as.matrix(cbind(loo_bs[[1]]$pointwise[, "elpd_loo"],
+                             loo_bs[[2]]$pointwise[, "elpd_loo"],
+                             loo_bs[[3]]$pointwise[, "elpd_loo"],
+                             loo_bs[[4]]$pointwise[, "elpd_loo"]))
+
+n_draws <- nrow(as.matrix(bsm[[1]]))
+
+# BS
+ypred_bs <- matrix(NA, nrow = n_draws, ncol = nobs(bsm[[1]]))
+for (d in 1:n_draws) {
+  k <- sample(1:length(w_bs), size = 1, prob = w_bs)
+  ypred_bs[d, ] <- posterior_predict(bsm[[k]], draws = 1)
+}
+
+y_bs <- colMeans(ypred_bs)
+d1 <- density(y_bs, kernel = c("gaussian"))$y
+d0 <- density(df$PV1READ, kernel = c("gaussian"))$y
+kld1 <- KLD(d1, d0)$sum.KLD.py.px
+
+# PBMA
+ypred_bma <- matrix(NA, nrow = n_draws, ncol = nobs(bsm[[1]]))
+for (d in 1:n_draws) {
+  k <- sample(1:length(w_pbma), size = 1, prob = w_pbma)
+  ypred_bma[d, ] <- posterior_predict(bsm[[k]], draws = 1)
+}
+
+y_bma <- colMeans(ypred_bma)
+d2 <- density(y_bma, kernel = c("gaussian"))$y
+kld2 <- KLD(d2, d0)$sum.KLD.py.px
+
+# PBMA+
+ypred_bmabb <- matrix(NA, nrow = n_draws, ncol = nobs(bsm[[1]]))
+for (d in 1:n_draws) {
+  k <- sample(1:length(w_pbmabb), size = 1, prob = w_pbmabb)
+  ypred_bmabb[d, ] <- posterior_predict(bsm[[k]], draws = 1)
+}
+
+y_bmabb <- colMeans(ypred_bmabb)
+d3 <- density(y_bmabb, kernel = c("gaussian"))$y
+kld3 <- KLD(d3, d0)$sum.KLD.py.px
+
+# BHS
+d_discrete <- 1
+X <- df[, c("ESCS","HOMEPOS","ICTRES",
+            "JOYREAD","PISADIFF","SCREADCOMP","SCREADDIFF",
+            "METASUM","GFOFAIL","MASTGOAL","SWBP","WORKMAST","ADAPTIVITY","COMPETE",
+            "PERFEED","TEACHINT","BELONG")] 
+
+stan_bhs <- list(X = X, N = nrow(X), d = ncol(X), d_discrete = d_discrete,
+                 lpd_point = lpd_point, K = ncol(lpd_point), tau_mu = 1,
+                 tau_sigma = 1, tau_discrete = .5, tau_con = 1)
+
+# Fit BHS model
+fit_bhs <- stan("bhs_stan.stan", data = stan_bhs, chains = 4, iter = 10000)
+
+# Extract weights
+wts_bhs <- rstan::extract(fit_bhs, pars = 'w')$w
+w_bhs_r <- apply(wts_bhs, c(2,3), mean) 
+w_bhs_m <- as.matrix(apply(wts_bhs, 3, mean))
+
+ypred_bhs_r <- matrix(NA, nrow = n_draws, ncol = nobs(bsm[[1]]))
+for (d in 1:n_draws) {
+  k <- sample(1:4, size = 1, prob = w_bhs_m)
+  ypred_bhs_r[d, ] <- posterior_predict(bsm[[k]], draws = 1)
+}
+
+y_bhs_r <- colMeans(ypred_bhs_r)
+d4 <- density(y_bhs_r, kernel = c("gaussian"))$y
+kld4 <- KLD(d4, d0)$sum.KLD.py.px
+
+# Combine results
+wr <- data.frame(as.matrix(w_bs), as.matrix(w_pbma), as.matrix(w_pbmabb), w_bhs_m)
+colnames(wr) <- c("bs","pbma", "pbmabb", "bhs")
+
+klds <- rbind(kld1, kld2, kld3, kld4)
+
+save(lpd_point, fit_bhs, wr, klds, bsm, loo_bs, 
+     file = "real_input.RData")
+
+
+# ::: FULL SAMPLE ::: #
+
+df <- df0 %>%
+  dplyr::select(SchoolID, CNTSTUID, Female, ESCS, METASUM, PERFEED, HOMEPOS, 
+                ADAPTIVITY, TEACHINT, ICTRES, ATTLNACT, COMPETE, JOYREAD,
+                WORKMAST, GFOFAIL, SWBP, MASTGOAL, BELONG, SCREADCOMP, 
+                PISADIFF, Public, PV1READ, SCREADDIFF)
+
+# Fit
+bsm <- list()
+loo_bs <- list()
+
+bsm[[1]] <- stan_lmer(
+  PV1READ ~ Female + ESCS + HOMEPOS + ICTRES + (1 + ICTRES|SchoolID), data = dt0, 
+  prior_intercept = student_t(3, 470, 100),
+  iter = 10000, chains = 4, adapt_delta=.999, thin=10)
+
+bsm[[2]] <- stan_lmer(
+  PV1READ ~ JOYREAD + PISADIFF + SCREADCOMP + SCREADDIFF + (1|SchoolID),
+  data = dt0, prior_intercept = student_t(3, 470, 100), iter = 10000, chains = 4,
+  adapt_delta=.999, thin=10)
+
+bsm[[3]] <- stan_lmer(
+  PV1READ ~ METASUM + GFOFAIL + MASTGOAL + SWBP + WORKMAST + ADAPTIVITY + COMPETE + (1|SchoolID),
+  data = dt0, prior_intercept = student_t(3, 470, 100), iter = 10000, chains = 4,
+  adapt_delta=.999, thin=10)
+
+bsm[[4]] <- stan_lmer(
+  PV1READ ~ PERFEED + TEACHINT + BELONG + (1 + TEACHINT|SchoolID),
+  data = dt0, prior_intercept = student_t(3, 470, 100), iter = 10000, chains = 4,
+  adapt_delta=.999, thin=10)
+
+# Compute LOO
+loo_bs[[1]] <- loo(log_lik(bsm[[1]]))
+loo_bs[[2]] <- loo(log_lik(bsm[[2]]))
+loo_bs[[3]] <- loo(log_lik(bsm[[3]]))
+loo_bs[[4]] <- loo(log_lik(bsm[[4]]))
+
+w_bs <- loo_model_weights(loo_bs, method = "stacking")
+w_pbma <- loo_model_weights(loo_bs, method = "pseudobma", BB=FALSE)
+w_pbmabb <- loo_model_weights(loo_bs, method = "pseudobma")
+
+# Extract pointwise densities
+lpd_point <- as.matrix(cbind(loo_bs[[1]]$pointwise[, "elpd_loo"],
+                             loo_bs[[2]]$pointwise[, "elpd_loo"],
+                             loo_bs[[3]]$pointwise[, "elpd_loo"],
+                             loo_bs[[4]]$pointwise[, "elpd_loo"]))
+
+n_draws <- nrow(as.matrix(bsm[[1]]))
+
+# BS
+ypred_bs <- matrix(NA, nrow = n_draws, ncol = nobs(bsm[[1]]))
+for (d in 1:n_draws) {
+  k <- sample(1:length(w_bs), size = 1, prob = w_bs)
+  ypred_bs[d, ] <- posterior_predict(bsm[[k]], draws = 1)
+}
+
+y_bs <- colMeans(ypred_bs)
+d1 <- density(y_bs, kernel = c("gaussian"))$y
+d0 <- density(dt0$PV1READ, kernel = c("gaussian"))$y
+kld1 <- KLD(d1, d0)$sum.KLD.py.px
+
+# PBMA
+ypred_bma <- matrix(NA, nrow = n_draws, ncol = nobs(bsm[[1]]))
+for (d in 1:n_draws) {
+  k <- sample(1:length(w_pbma), size = 1, prob = w_pbma)
+  ypred_bma[d, ] <- posterior_predict(bsm[[k]], draws = 1)
+}
+
+y_bma <- colMeans(ypred_bma)
+d2 <- density(y_bma, kernel = c("gaussian"))$y
+kld2 <- KLD(d2, d0)$sum.KLD.py.px
+
+# PBMA+
+ypred_bmabb <- matrix(NA, nrow = n_draws, ncol = nobs(bsm[[1]]))
+for (d in 1:n_draws) {
+  k <- sample(1:length(w_pbmabb), size = 1, prob = w_pbmabb)
+  ypred_bmabb[d, ] <- posterior_predict(bsm[[k]], draws = 1)
+}
+
+y_bmabb <- colMeans(ypred_bmabb)
+d3 <- density(y_bmabb, kernel = c("gaussian"))$y
+kld3 <- KLD(d3, d0)$sum.KLD.py.px
+
+# BHS
+d_discrete <- 1
+X <- dt0[, c("ESCS","HOMEPOS","ICTRES",
+             "JOYREAD","PISADIFF","SCREADCOMP","SCREADDIFF",
+             "METASUM","GFOFAIL","MASTGOAL","SWBP","WORKMAST","ADAPTIVITY","COMPETE",
+             "PERFEED","TEACHINT","BELONG")] 
+
+stan_bhs <- list(X = X, N = nrow(X), d = ncol(X), d_discrete = d_discrete,
+                 lpd_point = lpd_point, K = ncol(lpd_point), tau_mu = 1,
+                 tau_sigma = 1, tau_discrete = .5, tau_con = 1)
+
+fit_bhs <- stan("bhs_stan.stan", data = stan_bhs, chains = 4, iter = 10000)
+
+wts_bhs <- rstan::extract(fit_bhs, pars = 'w')$w
+w_bhs_r <- apply(wts_bhs, c(2,3), mean)
+w_bhs_m <- as.matrix(apply(wts_bhs, 3, mean))
+
+ypred_bhs_r <- matrix(NA, nrow = n_draws, ncol = nobs(bsm[[1]]))
+for (d in 1:n_draws) {
+  k <- sample(1:4, size = 1, prob = w_bhs_m)
+  ypred_bhs_r[d, ] <- posterior_predict(bsm[[k]], draws = 1)
+}
+
+y_bhs_r <- colMeans(ypred_bhs_r)
+d4 <- density(y_bhs_r, kernel = c("gaussian"))$y
+kld4 <- KLD(d4, d0)$sum.KLD.py.px
+
+# Combine results
+wr_full <- data.frame(as.matrix(w_bs), as.matrix(w_pbma), as.matrix(w_pbmabb), w_bhs_m)
+colnames(wr_full) <- c("bs","pbma", "pbmabb", "bhs")
+
+klds_full <- rbind(kld1, kld2, kld3, kld4)
+
+print(wr)
+print(wr_full)
+
+print(klds)
+print(klds_full)
---
+++ s1_sim.r
@@ -0,0 +1,212 @@
+# ::: STUDY 1 -- SIMULATION ::: #
+
+{
+  rm(list = ls())
+  library(loo)
+  library(rstan)
+  library(rstanarm)
+  library(LaplacesDemon)
+  library(tidyverse)
+  options(scipen = 999)
+  options(mc.cores = parallel::detectCores())
+  
+}
+
+# CHTC command
+args <- (commandArgs(TRUE))
+if(length(args)==0){
+  print("No arguments supplied.")
+}
+arguments <- commandArgs(trailingOnly=TRUE)
+rep <- as.numeric(arguments[[1]])     
+c <- as.numeric(arguments[[2]])       
+ni <- as.numeric(arguments[[3]])      
+nj <- as.numeric(arguments[[4]])      
+icc <- as.numeric(arguments[[5]])     
+sigma <- as.numeric(arguments[[6]])   
+
+set.seed(c)
+
+# DGP
+gamma00 <- 400
+gamma01 <- -20
+gamma02 <- 6
+gamma03 <- -2
+gamma04 <- 8
+u_0 <- sqrt(icc)        
+w_0 <- sqrt(1-icc)
+sigma <- sigma          # 5
+
+x <- matrix(99, nrow = ni*nj, ncol = 4)
+y <- matrix(99, nrow = ni*nj, ncol = 1) 
+r <- matrix(99, nrow = ni*nj, ncol = 1) 
+U <- matrix(99, nrow = ni*nj, ncol = 3)
+W <- matrix(99, nrow = ni*nj, ncol = 4)
+sim <- matrix(99, nrow = ni*nj, ncol = 9) 
+
+# Generate multilevel data
+df <- dgf(ni, nj, gamma00, gamma01, gamma02, gamma03, gamma04, u_0, w_0, sigma)
+
+# Extract leave-one-out pointwise densities
+extract_lpd <- function(x){
+  g <- x$pointwise[, "elpd_loo"]
+  return(g)
+}
+
+# 15 candidate models
+model_strings <- c(
+  "y ~ x1 + (1|i)",
+  "y ~ x2 + (1|i)",
+  "y ~ x3 + (1|i)",
+  "y ~ x4 + (1|i)",
+  "y ~ x1 + x2 + (1|i)",
+  "y ~ x1 + x3 + (1|i)",
+  "y ~ x1 + x4 + (1|i)",
+  "y ~ x2 + x3 + (1|i)",
+  "y ~ x2 + x4 + (1|i)",
+  "y ~ x3 + x4 + (1|i)",
+  "y ~ x1 + x2 + x3 + (1|i)",
+  "y ~ x1 + x2 + x4 + (1|i)",
+  "y ~ x1 + x3 + x4 + (1|i)",
+  "y ~ x2 + x3 + x4 + (1|i)",
+  "y ~ x1 + x2 + x3 + x4 + (1|i)"  # Model 15: True DGM
+)
+
+var_names <- c(
+  "x1", "x2", "x3", "x4", "x1_x2", "x1_x3", "x1_x4",
+  "x2_x3", "x2_x4", "x3_x4", "x1_x2_x3", "x1_x2_x4",
+  "x1_x3_x4", "x2_x3_x4", "x1_x2_x3_x4"
+)
+
+# Analysis
+for (k in 1:15){
+  # Fit Bayesian multilevel models
+  a <- stan_lmer(as.formula(model_strings[k]), data = df, 
+                 prior_intercept = student_t(3, 400, 10), 
+                 prior_covariance = decov(scale = 0.50), 
+                 iter = 10000, adapt_delta=.999, thin=10)
+  
+  # Extract log-likelihood
+  b <- log_lik(a, merge_chains = FALSE)
+  
+  # Store
+  assign(paste0("bms_", var_names[k]), a)
+  assign(paste0("loglik_", var_names[k]), b)
+  
+  # Check convergence
+  e <- a$stan_summary[, c("n_eff", "Rhat")] %>% colMeans()
+  assign(paste0("converge_", var_names[k]), e)
+}
+
+# Collect
+f <- ls(pattern = "bms_x", all.names = T)
+bms_all <- do.call(list, mget(f))
+
+d <- ls(pattern = "loglik_x", all.names = T)
+loglik_all <- do.call(list, mget(d))
+
+# Compute LOO for all models
+time_loo <- system.time(loo_bms <- lapply(loglik_all, loo, cores = 4))
+
+# Compute ensemble weights
+time_bs <- time_loo + system.time(
+  w_bs <- loo::loo_model_weights(loo_bms, method = "stacking"))
+
+time_pbma <- time_loo + system.time(
+  w_pbma <- loo::loo_model_weights(loo_bms, method = "pseudobma", BB = FALSE))
+
+time_pbmabb <- time_loo + system.time(
+  w_pbmabb <- loo::loo_model_weights(loo_bms, method = "pseudobma"))
+
+# Generate predictions
+n_draws <- nrow(as.matrix(bms_all[[1]]))
+
+# BS
+ypred_bs <- matrix(NA, nrow = n_draws, ncol = nobs(bms_all[[1]]))
+for (d in 1:n_draws) {
+  k <- sample(1:length(w_bs), size = 1, prob = w_bs)
+  ypred_bs[d, ] <- posterior_predict(bms_all[[k]], draws = 1)
+}
+
+y_bs <- colMeans(ypred_bs)
+d1 <- density(y_bs, kernel = c("gaussian"))$y
+d0 <- density(df$y, kernel = c("gaussian"))$y
+kld1 <- KLD(d1, d0)$sum.KLD.py.px
+
+# PBMA
+ypred_pbma <- matrix(NA, nrow = n_draws, ncol = nobs(bms_all[[1]]))
+for (d in 1:n_draws) {
+  k <- sample(1:length(w_pbma), size = 1, prob = w_pbma)
+  ypred_pbma[d, ] <- posterior_predict(bms_all[[k]], draws = 1)
+}
+
+y_pbma <- colMeans(ypred_pbma)
+d2 <- density(y_pbma, kernel = c("gaussian"))$y
+kld2 <- KLD(d2, d0)$sum.KLD.py.px
+
+# PBMA+
+ypred_pbmabb <- matrix(NA, nrow = n_draws, ncol = nobs(bms_all[[1]]))
+for (d in 1:n_draws) {
+  k <- sample(1:length(w_pbmabb), size = 1, prob = w_pbmabb)
+  ypred_pbmabb[d, ] <- posterior_predict(bms_all[[k]], draws = 1)
+}
+
+y_pbmabb <- colMeans(ypred_pbmabb)
+d3 <- density(y_pbmabb, kernel = c("gaussian"))$y
+kld3 <- KLD(d3, d0)$sum.KLD.py.px
+
+# BHS
+X <- df %>% select(x1, x2, x3, x4)
+N <- nrow(X)
+d <- ncol(X)
+lpd_point <- do.call(cbind, lapply(loo_bms, extract_lpd))
+K <- ncol(lpd_point)
+
+dt_bhs <- list(N = N, d = d, K = K, X = X, 
+               lpd_point = lpd_point, tau_mu = 1, tau_con = 1)
+
+time_bhs <- system.time(
+  fit_bhs <- stan("bhs_con.stan", data = dt_bhs, iter = 10000, save_warmup = FALSE))
+
+wts_bhs <- rstan::extract(fit_bhs, pars = 'w')$w
+w_bhs <- apply(wts_bhs, c(2,3), mean)
+w_bhs_m <- as.matrix(apply(wts_bhs, 3, mean))
+
+ypred_bhs <- matrix(NA, nrow = n_draws, ncol = nobs(bms_all[[1]]))
+for (d in 1:n_draws) {
+  k <- sample(1:15, size = 1, prob = w_bhs_m)
+  ypred_bhs[d, ] <- posterior_predict(bms_all[[k]], draws = 1)
+}
+
+y_bhs <- colMeans(ypred_bhs)
+d4 <- density(y_bhs, kernel = c("gaussian"))$y
+kld4 <- KLD(d4, d0)$sum.KLD.py.px
+
+# Combine results
+ws <- data.frame(as.matrix(w_bs), as.matrix(w_pbma), as.matrix(w_pbmabb), w_bhs_m)
+klds <- rbind(kld1, kld2, kld3, kld4) %>% as.data.frame()
+
+# Timing
+time <- rbind(t(data.matrix(time_bs)), t(data.matrix(time_pbma)), 
+              t(data.matrix(time_pbmabb)), t(data.matrix(time_bhs))) %>% as.data.frame()
+
+rnames <- c("bs","pbma", "pbmabb", "bhs")  
+rownames(klds) <- rnames
+colnames(ws) <- rnames
+rownames(time) <- rnames
+
+# Save
+assign(paste0("rep_", rep, "_seed", c, "_", ni, "_", nj, "_sd", icc, "_sigma", sigma, "_ws"), ws)
+assign(paste0("rep_", rep, "_seed", c, "_", ni, "_", nj, "_sd", icc, "_sigma", sigma, "_kld"), klds)
+assign(paste0("rep_", rep, "_seed", c, "_", ni, "_", nj, "_sd", icc, "_sigma", sigma, "_time"), time)
+
+sw <- paste0("rep_", rep, "_seed", c, "_", ni, "_", nj, "_sd", icc, "_sigma", sigma, "_ws")
+dlk <- paste0("rep_", rep, "_seed", c, "_", ni, "_", nj, "_sd", icc, "_sigma", sigma, "_kld")
+emit <- paste0("rep_", rep, "_seed", c, "_", ni, "_", nj, "_sd", icc, "_sigma", sigma, "_time")
+
+save(list = c(sw,dlk,emit), file = paste0("rep_", rep, "_seed", c, "_", ni, "_", nj, "_sd", icc, "_sigma", sigma, "_out.RData"))
+
+file.copy(from = paste0("rep_", rep, "_seed", c, "_", ni, "_", nj, "_sd", icc, "_sigma", sigma, "_out.RData"),
+          to = paste0("/staging/mhuang233/rep_", rep, "_seed", c, "_", ni, "_", nj, "_sd", icc, "_sigma", sigma, "_out.RData"))
+
+file.remove(paste0("rep_", rep, "_seed", c, "_", ni, "_", nj, "_sd", icc, "_sigma", sigma, "_out.RData"))
